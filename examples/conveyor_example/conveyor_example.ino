99c8a2ca6bfbf0e578a4d31741434a88aef11aed
haunt07 <fachri111@gmail.com>
2026-02-03 10:09:55 +0700

    add examples

---
+++ examples/conveyor_example/conveyor_example.ino
@@ -0,0 +1,17 @@
+#include <robot_conveyor.h>
+
+Conveyor conve;
+void setup(){
+    Serial.begin(115200);
+    conve.begin(12);
+    Serial.println("Inisialisasi sistem");
+}
+
+void loop(){
+    Serial.println("Conveyor on");
+    conve.On();
+    delay(5000);
+    Serial.println("Conveyor off");
+    conve.Off();
+    delay(5000);
+}
